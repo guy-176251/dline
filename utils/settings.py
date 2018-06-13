b848a3712431dc149a03b1b60f5d9e2c0b6ac23d
Nat Osaka <natthetupper@gmail.com>
2018-06-13 12:08:42 -0400

    Current config check added

--- utils/settings.py
+++ utils/settings.py
@@ -1,7 +1,10 @@
 import os
 import sys
-from yaml import safe_load
+from yaml import safe_load, YAMLError
 from blessings import Terminal
+
+class OutdatedConfigException(Exception):
+    pass
 
 settings = ""
 
@@ -11,57 +14,72 @@ def copy_skeleton():
         from shutil import copyfile
         if not os.path.exists(os.getenv("HOME") + "/.config/Discline"):
             os.mkdir(os.getenv("HOME") + "/.config/Discline")
-        
+
         if os.path.exists(os.getenv("HOME") + "/.config/Discline/config"):
             try:
                 os.remove(os.getenv("HOME") + "/.config/Discline/config")
             except:
                 pass
 
-        copyfile("res/settings-skeleton.yaml", os.getenv("HOME") + "/.config/Discline/config", follow_symlinks=True) 
+        copyfile("res/settings-skeleton.yaml", os.getenv("HOME") + "/.config/Discline/config", follow_symlinks=True)
         print(term.green("Skeleton copied!" + term.normal))
         print(term.cyan("Your configuration file can be found at ~/.config/Discline"))
 
-    except KeyboardInterrupt: 
+    except KeyboardInterrupt:
         print("Cancelling...")
         quit()
     except SystemExit:
         quit()
     except Exception as e:
-        print(term.red("Error creating skeleton file:"), e)
+        print("ERROR: Could not create skeleton file:", e)
         quit()
 
-def load_config(path):
+def load_config():
+    arg = ""
+    path = ""
+    try:
+        arg = sys.argv[1]
+    except IndexError:
+        pass
+    if not path:
+        try:
+            if arg == "--config":
+                path = sys.argv[2]
+            else:
+                path = os.getenv("HOME") + "/.config/Discline/config"
+        except IndexError:
+            print("ERROR: No path specified.")
+            quit()
     global settings
-    with open(path) as f:
-        settings = safe_load(f)
- 
+    try:
+        with open(path) as f:
+            settings = safe_load(f)
+        if "show_user_bar" not in settings:
+            raise OutdatedConfigException
+    except YAMLError:
+        print("ERROR: Invalid config. Check and try again.")
+        quit()
+    except OutdatedConfigException:
+        print("ERROR: Outdated config. Please update your config with --copy-skeleton and run again.")
+        quit()
+    except OSError:
+        print("ERROR: Could not open config file.")
+        quit()
+    except:
+        print("ERROR: Could not load config.")
+        quit()
+
 arg = ""
-try: 
+try:
     arg = sys.argv[1]
-except IndexError: 
+except IndexError:
     pass
 
+doLoad = True
 if arg == "--store-token" or arg == "--token":
-    pass
+    doLoad = False
 elif arg == "--skeleton" or arg == "--copy-skeleton":
     copy_skeleton()
     quit()
-elif arg == "--config":
-    try:
-        load_config(sys.argv[2])
-    except IndexError:
-        print("No path provided?")
-        quit()
-    except:
-        print("Invalid path to config entered.")
-        quit()
-else:
-    try:
-        load_config(os.getenv("HOME") + "/.config/Discline/config")
-    except:
-        try:
-            load_config(os.getenv("HOME") + "/.Discline")
-        except:
-            print(term.red("ERROR: could not get settings."))
-            quit()
+if doLoad:
+    load_config()
